7a5f86f83790bc1be2c2dcd01e22d9e55f7f7618
Elliot Su <sodinfeliz@gmail.com>
2021-03-30 18:24:08 +0800

    Update `table_generate.py` file.

--- script/table_generate.py
+++ script/table_generate.py
@@ -1,7 +1,18 @@
 from pathlib import Path
 
+def extract(path: Path):
+    num, title = str(path.stem).split(sep='_', maxsplit=1)
+    title = title.replace('_', ' ')
 
 
-for path in Path('python/').glob('*.py'):
-    print(path.stem)
+
+def main():
+    src_paths = list(Path('python/').glob('*.py'))[::-1]
+    with open('README_table.md', 'w') as f:
+        f.write('| # | Title | Solution | Difficulty |\n')
+        f.write('|---| ----- | -------- | ---------- |\n')
+
+
+
+
 
